d2b45b92559aa891b1e808cf081aed5b38b068cf
Starinin Andrey <starininandrey@gmail.com>
2023-08-26 15:51:51 +0300

    Initial commit

---
+++ Calculator/Calc.cs
@@ -0,0 +1,30 @@
+﻿using _Logger_.Library;
+
+namespace Calculator;
+
+public class Calc
+{
+    public ILogger Logger { get; set; }
+
+    public int Add(int x, int y)
+    {
+        Logger.LogInfo($"Сложение {x} и {y}");
+        var result = x + y;
+        Logger.LogSuccess($"{x} + {y} = {result}");
+        return result;
+    }
+    
+    public int Div(int x, int y)
+    {
+        Logger.LogInfo($"Деление {x} и {y}");
+        if (y == 0)
+        {
+            Logger.LogError("Делить на ноль нельзя");
+            throw new DivideByZeroException();
+        }
+        
+        var result = x / y;
+        Logger.LogSuccess($"{x} / {y} = {result}");
+        return result;
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using _Logger_.Library;
+using Calculator;
+
+var calc = new Calc()
+{
+    Logger = new FileLog("test.log", "test.log", "test.log", "test.log")
+};
+
+try
+{
+    calc.Add(5, 0);
+    calc.Div(5, 0);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
